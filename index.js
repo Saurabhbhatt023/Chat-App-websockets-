516be660e0bee88002b1d8d145ff945449f3ce41
Saurabh <saurabhbhatt023@gmail.com>
2025-04-03 20:50:21 +0530

    Initial commit - Chat App with WebSockets

---
+++ index.js
@@ -0,0 +1,8 @@
+const http  = require('http')
+const express = require('express')
+
+  const app = express()
+ const server  = http.createServer(app)
+
+ app.use(express.static("/public"))
+ server.listen(3000, () => console.log('Server Started') )
